0419dbb9f7348b97c39a2b3c099bcfff5332283d
muktha gk <gkmukthaa.xworkz@gmail.com>
2024-06-24 21:13:42 +0530

    this is third commit

---
+++ Marks.java
@@ -0,0 +1,10 @@
+public class Marks {
+
+	static int kannada_Marks = 78;
+	static int english_Marks = 89;
+	static int maths_Marks = 90;
+	static int hindi_Marks = 91;
+	static int science_Marks = 95;
+	static int social_Marks = 88;
+	static int add = kannada_Marks+english_Marks+maths_Marks+hindi_Marks+science_Marks+social_Marks;
+	}
