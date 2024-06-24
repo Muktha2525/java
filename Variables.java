e6a6679dcf9ec93e71314966c6a7d886ebc873d8
muktha gk <gkmukthaa.xworkz@gmail.com>
2024-06-24 21:15:10 +0530

    this is fifth commit

---
+++ Variables.java
@@ -0,0 +1,12 @@
+public class Variables {
+    // Declare 9 static variables
+    static int var1 = 1;
+    static double var2 = 2.0;
+    static float var3 = 3.0f;
+    static long var4 = 4L;
+    static char var5 = 'A';
+    static boolean var6 = true;
+    static String var7 = "Hello";
+    static short var8 = 8;
+    static byte var9 = 9;
+}
