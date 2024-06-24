a3d2cee7d3b45847a1b888c7ff7b52ca496561ad
muktha gk <gkmukthaa.xworkz@gmail.com>
2024-06-24 21:14:29 +0530

    this is fourth commit

---
+++ CourseFee.java
@@ -0,0 +1,7 @@
+public class CourseFee{
+    
+       static double courseFee = 12000.0;
+        static double gstRate = 18.0;
+		static  double gstAmount= courseFee*gstRate/100;
+		static double totalFee = courseFee+ gstAmount;
+}
