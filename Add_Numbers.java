9fd5d542544761b45599627ff3dee21556391aa4
muktha gk <gkmukthaa.xworkz@gmail.com>
2024-06-24 21:10:44 +0530

    this is second commit

---
+++ Add_Numbers.java
@@ -0,0 +1,22 @@
+public class Add_Numbers{
+	
+	static int a = 10;
+	static int b = 20;
+ 	static int add = a + b;
+	
+	public static void main(String[]args){
+		
+		System.out.println("ADDING TWO NUMBERS");
+		System.out.println("First No is :" + a);
+		System.out.println("Second No is  :" + b);
+		System.out.println("Sum of two no is:"+ add);
+		
+	if (a == add){
+		System.out.println("The sum of the two numbers is equal to the  first_No");
+	}
+	else{
+		System.out.println("The sum of the two numbers is not equal to the first_No");	
+		}
+	}
+}
+
---
+++ Building.java
@@ -0,0 +1,28 @@
+public class Building {
+
+    public static void main(String[] args) {
+       
+        String buildingName = "SAM";
+        int floors = 50;
+        double heightInMeters = 250.5;
+        boolean isCommercial = true;
+        char buildingCode = 'S';
+        int constructionYear = 2020;
+        double constructionCostInMillions = 350.75;
+        boolean isEcoFriendly = true;
+        char energyEfficiencyRating = 'A';
+        String architectName = "KRISH";
+        
+        
+        System.out.println("Building Name: " + buildingName);
+        System.out.println("Number of Floors: " + floors);
+        System.out.println("Height (in meters): " + heightInMeters);
+        System.out.println("Is Commercial: " + isCommercial);
+        System.out.println("Building Code: " + buildingCode);
+        System.out.println("Construction Year: " + constructionYear);
+        System.out.println("Construction Cost (in millions): " + constructionCostInMillions);
+        System.out.println("Is Eco-friendly: " + isEcoFriendly);
+        System.out.println("Energy Efficiency Rating: " + energyEfficiencyRating);
+        System.out.println("Architect Name: " + architectName);
+    }
+}
