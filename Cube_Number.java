1edfdd00b9de1c208bbf85d066baf784d7dbf6df
muktha gk <gkmukthaa.xworkz@gmail.com>
2024-06-27 20:42:05 +0530

    Wednesday task

---
+++ Cube_Number.java
@@ -0,0 +1,13 @@
+public class Cube_Number {
+
+    public static void main(String[] args) {
+       
+        int number = 5;
+
+        
+        int cube = number * number * number;
+
+       
+        System.out.println("The cube of " + number + " is: " + cube);
+    }
+}
