90b1ed176d4c28c513f4fa39a2083dc7df69e914
muktha gk <gkmukthaa.xworkz@gmail.com>
2024-06-30 22:57:34 +0530

    Friday task

---
+++ Even_And_Odd.java
@@ -0,0 +1,9 @@
+public class Even_And_Odd{
+
+public static void info(){
+
+	System.out.println("Even no : 2,4,6,8,10");
+	System.out.println("Odd no: 1,3,5,7,9");
+
+}
+}
---
+++ Even_And_Odd_Runner.java
@@ -0,0 +1,11 @@
+public class Even_And_Odd_Runner{
+
+public static void main(String args[]){
+
+
+System.out.println("Invoking main method");
+System.out.println("Even and Odd Numbers");
+ Even_And_Odd.info();
+
+}
+}
