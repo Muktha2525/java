afe9637cae61a143b0504670a8aa61ce7b7d3c99
muktha gk <gkmukthaa.xworkz@gmail.com>
2024-06-30 22:58:40 +0530

    Friday task

---
+++ Hospitality.java
@@ -0,0 +1,8 @@
+ public class Hospitality{
+	public static void patientsinfo(){
+		
+		System.out.println("Location in Jayanagara");
+		System.out.println("Jayadeva");
+       
+	}
+}
---
+++ HospitalityRunner.java
@@ -0,0 +1,8 @@
+public class HospitalityRunner{
+	public  static void main(String[]args){
+	
+	System.out.println("It is a good hospital");
+	
+	Hospitality.patientsinfo();	
+	}
+}
