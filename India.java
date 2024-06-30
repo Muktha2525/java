94921f516f965a41d0ab3f19ca73466dfad47dd4
muktha gk <gkmukthaa.xworkz@gmail.com>
2024-06-30 23:00:04 +0530

    Friday task

---
+++ India.java
@@ -0,0 +1,9 @@
+public class India{
+
+public static void info(){
+
+System.out.println("India is a democratic country");
+System.out.println("Total states are 28");
+
+}
+}
---
+++ IndiaRunner.java
@@ -0,0 +1,11 @@
+public class IndiaRunner{
+
+public static void main(String args[]){
+
+
+System.out.println("Invoking main method");
+System.out.println("Indian Country");
+India.info();
+
+}
+} 
