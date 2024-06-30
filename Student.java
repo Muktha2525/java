cd419cf2697c7e6b83d2ae4f078a9c7e58dea0c6
muktha gk <gkmukthaa.xworkz@gmail.com>
2024-06-30 23:06:54 +0530

    Friday task

---
+++ Student.java
@@ -0,0 +1,9 @@
+public class Student {
+
+public static void info(){
+
+	System.out.println("Student Name: Kruthika");
+	System.out.println("USN: 4SM20CS036");
+
+}
+}
---
+++ StudentRunner.java
@@ -0,0 +1,11 @@
+public class StudentRunner{
+
+	public static void main(String args[]){
+
+
+		System.out.println("Invoking main method");
+		System.out.println("Student Information");
+		Student.info();
+
+		}
+	}
