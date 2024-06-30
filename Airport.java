bdc7bb55c10360bad8320ef62d2a33d3adb3ea9f
muktha gk <gkmukthaa.xworkz@gmail.com>
2024-06-30 22:52:37 +0530

    Friday task

---
+++ Airport.java
@@ -0,0 +1,8 @@
+ public class Airport{
+	public static void info(){
+		
+		System.out.println("From Banglore to Mumbai");
+		System.out.println("On 13th july 2024");
+       
+	}
+}
---
+++ AirportRunner.java
@@ -0,0 +1,8 @@
+public class AirportRunner{
+	public  static void main(String[]args){
+	
+	System.out.println("Airport Details");
+	
+	Airport.info();	
+	}
+}
