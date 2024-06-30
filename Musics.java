b4439ba7931801b237ab86bb87e4e30bb1c080c6
muktha gk <gkmukthaa.xworkz@gmail.com>
2024-06-30 23:03:46 +0530

    Friday task

---
+++ Musics.java
@@ -0,0 +1,8 @@
+ public class Musics{
+	public static void info(){
+		
+		System.out.println("It is Melodious");
+		System.out.println("It is good to listen");
+       
+	}
+}
---
+++ MusicsRunner.java
@@ -0,0 +1,8 @@
+public class MusicsRunner{
+	public  static void main(String[]args){
+	
+	System.out.println("Musics Details");
+	
+	Musics.info();	
+	}
+}
