4f28b72caf27d76589303420027843ac3c63a520
muktha gk <gkmukthaa.xworkz@gmail.com>
2024-06-30 22:56:09 +0530

    Friday task

---
+++ District.java
@@ -0,0 +1,8 @@
+ public class District{
+	public static void info(){
+		
+		System.out.println("District: Chitradurga");
+		System.out.println("It is an Historical City");
+       
+	}
+}
---
+++ DistrictRunner.java
@@ -0,0 +1,8 @@
+public class DistrictRunner{
+	public  static void main(String[]args){
+	
+	System.out.println("Chitradurga");
+	
+	District.info();	
+	}
+}
