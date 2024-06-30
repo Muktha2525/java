fbb94e48d9d2643dfa5b1ea0c0941760a5d35fde
muktha gk <gkmukthaa.xworkz@gmail.com>
2024-06-30 23:02:01 +0530

    Friday task

---
+++ MathOperations.java
@@ -0,0 +1,8 @@
+ public class Hospitality{
+	public static void info(){
+		
+		System.out.println("Addition");
+		System.out.println("Multiplication");
+       
+	}
+}
---
+++ MathOperationsRunner.java
@@ -0,0 +1,8 @@
+public class MathOperationsRunner{
+	public  static void main(String[]args){
+	
+	System.out.println("Substraction");
+	System.out.println("Division");
+	MathOperations.info();	
+	}
+}
