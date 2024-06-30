7a1ee2d578a7253420b05d190470245ce57a84b1
muktha gk <gkmukthaa.xworkz@gmail.com>
2024-06-30 23:02:49 +0530

    Friday task

---
+++ MetroStation.java
@@ -0,0 +1,9 @@
+public class MetroStation{
+
+public static void info(){
+
+	System.out.println("Ticket counter");
+	System.out.println("GREEN LINE");
+
+}
+}
---
+++ MetroStationRunner.java
@@ -0,0 +1,11 @@
+public class MetroStationRunner{
+
+	public static void main(String args[]){
+
+
+		System.out.println("Invoking main method");
+		System.out.println("Metro Station Information");
+		MetroStation.info();
+
+		}
+	}
